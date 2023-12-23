086dfae642653712a5e0a747e5309201030aae06
Lucas Matheus <lucasmatheus@gmail.com>
2023-12-23 00:17:21 -0300

    feat: added share invite public page
    Signed-off-by: Lucas Matheus <lucasmatheus@gmail.com>

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 
 import useAuth from "@/hooks/useAuth";
 import routes from ".";
@@ -9,8 +9,19 @@ interface PublicRouteProps {
   children: React.ReactNode;
 }
 
+const canAccessPage = (pathToVerify: string, currentPath: string) => {
+  if (typeof window === "undefined") return;
+
+  return currentPath.includes(pathToVerify);
+};
+
 const PublicRoute = ({ children }: PublicRouteProps) => {
+  const pathName = usePathname();
   const { isLoggedIn } = useAuth();
+
+  if (canAccessPage("convidados/confirmar-presenca", pathName)) {
+    return <>{children}</>;
+  }
 
   if (isLoggedIn) {
     redirect(routes.private.home);
